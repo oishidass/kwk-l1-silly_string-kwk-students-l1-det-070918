32db8d6fe33ed26a644e5de87e46514b2f2a3dfc
oishidass <oishidass1234@gmail.com>
2018-07-10 18:01:54 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -7,15 +7,15 @@ puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-favorite_word_in_all_caps = your_favorite_word.upcase # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = your_favorite_word.upcase 
 
-puts  #{favorite_word_in_all_caps}
+puts "my favorite word is #{favorite_word_in_all_caps}"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
 phrase = "IM NOT SHOUTING"
 
-lowercased_phrase = phrase
+lowercased_phrase = phrase.downcase
 
 puts lowercased_phrase
 
@@ -23,13 +23,13 @@ puts lowercased_phrase
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word.size
 
 puts "There are #{letter_count} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "wow" + "mom"
 
 new_letter_count = bigger_word.size
 
@@ -37,7 +37,7 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".capitalize # Put a . after the string to capitalize it properly.
 
 puts proper_sentence
 
